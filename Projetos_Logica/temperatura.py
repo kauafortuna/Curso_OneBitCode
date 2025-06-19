99f0e3a7307eeb028d25c85f41c41a38447f4688
Kauã Souza Fortuna <kauasouzafotuna@gmail.com>
2025-06-19 20:17:23 -0300

    pequena mudança no arquivo temperatura

---
+++ Projetos_Logica/temperatura.py
@@ -0,0 +1,14 @@
+
+temperatura =29
+
+if temperatura >=31:
+    print("Está muito quente!")
+
+if temperatura <=30:
+    print("Temperatura Agradável")
+     
+if temperatura <=16:
+    print("Está Frio, se agasalhe bem!")
+
+else:
+    print ("A TEMPERATURA ESTÁ ABAIXO DO NORMAL, SE AGASALHE BEM!!!")
